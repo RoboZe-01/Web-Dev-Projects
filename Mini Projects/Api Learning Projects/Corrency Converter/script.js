140f181bb806fdb486cc3922d8012b52936d7cd8
RoboZe <roboticsxzero@gmail.com>
2025-07-04 17:06:20 +0530

    CSS Completed Javascript Started

---
+++ Mini Projects/Api Learning Projects/Corrency Converter/script.js
@@ -0,0 +1,6 @@
+const fromAmount = document.getElementById("fromAmount");
+const fromCurrency = document.getElementById("fromCurrency");   
+const toAmount = document.getElementById("toAmount");
+const toCurrency = document.getElementById("toCurrency");   
+const convertButton = document.getElementById("convertButton");
+const result = document.querySelector(".result");   
