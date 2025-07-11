8ae0a5326546d51cb6c1d876579cdadc01f189b8
RoboZe <roboticsxzero@gmail.com>
2025-07-11 21:23:56 +0530

    Learned to make simple component in React

--- React Projects/Shoes Website/shoes-website/src/App.jsx
+++ React Projects/Shoes Website/shoes-website/src/App.jsx
@@ -1,20 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = ()=> {
   return (
-    <>
-      <div>
-       
-       <h1>Hello World </h1>
-      </div>
-     
-    </>
+    <div>
+      <h1>This is my first app </h1>
+    </div>
   )
 }
-
-export default App
+export default App;
