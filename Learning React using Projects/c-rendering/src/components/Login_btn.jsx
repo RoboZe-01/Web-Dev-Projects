e24768a19be5e65c5082ca14a7d5053540039188
RoboZe <roboticsxzero@gmail.com>
2025-07-18 17:10:11 +0530

    conditional rerendering learned

---
+++ Learning React using Projects/c-rendering/src/components/Login_btn.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Login_btn = () => {
+  return (
+  <button>Login</button>
+
+)
+}
+
+export default Login_btn
---
+++ Learning React using Projects/c-rendering/src/components/logot_btn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const logot_btn = () => {
+  return (
+    <button>Log Out</button>
+  )
+}
+
+export default logot_btn
