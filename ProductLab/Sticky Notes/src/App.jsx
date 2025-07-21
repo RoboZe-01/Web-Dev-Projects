e0353f58621ed008b707853ccc0f3f8ce4783d6f
RoboZe <roboticsxzero@gmail.com>
2025-07-21 22:17:06 +0530

    Basic Setup done

--- ProductLab/Sticky Notes/src/App.jsx
+++ ProductLab/Sticky Notes/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import NotesPage from './Pages/NotesPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <NotesPage />
     </>
   )
 }
-
 export default App
---
+++ ProductLab/Sticky Notes/src/Pages/NotesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotesPage = () => {
+  return (
+    <> 
+    notePage
+    
+    </>
+  )
+}
+
+export default NotesPage
