b7def7b84010c32ad80d1e186a92f152e7612b72
RoboZe <roboticsxzero@gmail.com>
2025-07-21 22:31:37 +0530

    Basic Styling and fake data to work with added

--- ProductLab/Sticky Notes/src/App.jsx
+++ ProductLab/Sticky Notes/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    <NotesPage />
+   <NotesPage />
     </>
   )
 }
--- ProductLab/Sticky Notes/src/Pages/NotesPage.jsx
+++ ProductLab/Sticky Notes/src/Pages/NotesPage.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const NotesPage = () => {
   return (
     <> 
-    notePage
+   NotePage 
+
     
     </>
   )
