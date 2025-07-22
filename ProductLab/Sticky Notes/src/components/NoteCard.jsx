de5291999aefc082186a6d9aeff095deaae9abb7
RoboZe <roboticsxzero@gmail.com>
2025-07-22 08:30:33 +0530

    Added a feature to move the notecard over a screen

--- ProductLab/Sticky Notes/src/components/NoteCard.jsx
+++ ProductLab/Sticky Notes/src/components/NoteCard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Trash from '../icons/Trash';
-import { useRef  ,useEffect} from 'react';
+import { useRef  ,useEffect , useState} from 'react';
 
 const NoteCard = ({note}) => {
     const body = JSON.parse(note.body);
-    const position = JSON.parse(note.position);
+    const [position , setPosition] = useState(JSON.parse(note.position));
     const colors = JSON.parse(note.colors);
+
+    let mouseStartPos = { x: 0, y: 0 };
+    const cardRef = useRef(null);
+
 
     const textAreaRef = useRef(null);
     useEffect(()=>{
@@ -19,15 +23,47 @@ const NoteCard = ({note}) => {
 
     }
 
+    const mouseDown = (e) => {
+      mouseStartPos.x = e.clientX 
+      mouseStartPos.y = e.clientY
+
+      document.addEventListener('mousemove' , mouseMove)
+    };
+ 
+    
+    const mouseMove =(e) => {
+      const mouseMoveDir = {
+        x : mouseStartPos.x - e.clientX,
+        y : mouseStartPos.y - e.clientY,
+      };
+      
+      console.log('MouseMoveDir : ' ,mouseMoveDir)
+       mouseStartPos.x = e.clientX 
+       mouseStartPos.y = e.clientY
+
+       setPosition ({
+        x: cardRef.current.offsetLeft - mouseMoveDir.x,
+        y: cardRef.current.offsetTop - mouseMoveDir.y,
+       })
+
+    };
+
+
 
   return (
     // Main card jsx
-    <div className='card' style={{backgroundColor: colors.colorBody ,
+    <div 
+    ref={cardRef}
+    className='card' style={{backgroundColor: colors.colorBody ,
       left : `${position.x}px`,
       top : `${position.y}px`
      }}>
        
-       <div className="card-header" style={{backgroundColor: colors.colorHeader }}>
+       <div
+        onMouseDown={mouseDown}
+        className="card-header" 
+        style={{backgroundColor: colors.colorHeader }}
+       >
         <Trash />
        </div>
        
