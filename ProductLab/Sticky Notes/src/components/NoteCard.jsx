e98bb9a056419cfc35d063c80c3dfffac2ff1cf3
RoboZe <roboticsxzero@gmail.com>
2025-07-22 08:09:44 +0530

    Added regrow feature to card to change its size accordingly to the text

--- ProductLab/Sticky Notes/src/components/NoteCard.jsx
+++ ProductLab/Sticky Notes/src/components/NoteCard.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
+import Trash from '../icons/Trash';
+import { useRef  ,useEffect} from 'react';
 
 const NoteCard = ({note}) => {
     const body = JSON.parse(note.body);
     const position = JSON.parse(note.position);
     const colors = JSON.parse(note.colors);
+
+    const textAreaRef = useRef(null);
+    useEffect(()=>{
+      autoGrow(textAreaRef);
+    },[])
+
+    const autoGrow=(textArea) => {
+          const {current} = textAreaRef;
+          current.style.height = "auto"; // Reset the height
+          current.style.height = current.scrollHeight + "px"; // Set the new height
+
+    }
+
+
   return (
-    <div className='card' style={{backgroundColor: colors.colorBody}}>
-       <div className="card-header" style={{backgroundColor: colors.colorHeader }}></div>
+    // Main card jsx
+    <div className='card' style={{backgroundColor: colors.colorBody ,
+      left : `${position.x}px`,
+      top : `${position.y}px`
+     }}>
+       
+       <div className="card-header" style={{backgroundColor: colors.colorHeader }}>
+        <Trash />
+       </div>
+       
        <div className="card-body">
         <textarea name="" id="" defaultValue={body}
+        onInput={()=>{
+          autoGrow(textAreaRef);
+        }}
+        ref ={textAreaRef}
         style={{color : colors.colorText}}>
           
           
