af51449d8a0778325d6c7b5be24fd09acc513b66
RoboZe <roboticsxzero@gmail.com>
2025-07-26 02:08:36 +0530

    Signing Off

--- Learning React using Projects/React Routing Dom/src/App.jsx
+++ Learning React using Projects/React Routing Dom/src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react"
+import { Routes } from "react-router"
+import { createBrowserRouter , RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter(
+[
+  {
+    
+  },
+  {},
+  {}
+]
+
+);
 function App() {
   
 
   return (
     <>
-    
+    <h1>Learning React Router DOM</h1>
     
     </>
 
