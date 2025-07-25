9ba1b0bbe1aedda1736b3d2b9c5bf38242c8824a
RoboZe <roboticsxzero@gmail.com>
2025-07-25 15:17:25 +0530

    Basics of useeffect done

--- Learning React using Projects/UseEffect Hook/Notes.md
+++ Learning React using Projects/UseEffect Hook/Notes.md
@@ -16,3 +16,40 @@
 `useEffect(() => {
     <! --Write the function of the useeffect-->
  } , [dependency array ( Varible on which it is dependent)])`
+
+
+
+ We can control something when some specific state is updated
+
+ `For Example
+ useEffect(() =>{
+    // callOnce();\
+    CounterFunction()
+  },[counter])
+
+  function CounterFunction (){
+    console.log(`Counter ${counter}`)
+    This function only activate once counter state is updated`
+
+
+
+## Handeling Dependencies
+
+1. `useEffect(() =>{
+    // Calls Every time
+  })`
+
+2. `useEfect((=>{
+   // Calls only once
+    } ,[])`  
+
+3. `useEfect(()=>{
+    // updates when specific states is updates
+  } ,[state])`
+
+4. useEffect(()=>{
+    //calls on changon both the states
+    }[state1,state2])
+5. useEffect(()=>{
+    //calls on changon both the props
+    }[prop1,prop2])
--- Learning React using Projects/UseEffect Hook/src/App.jsx
+++ Learning React using Projects/UseEffect Hook/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+ 
+  const [counter , setCounter]= useState(0);
+  const [data , setData]= useState(0);
+
+  useEffect(() =>{
+    // callOnce();\
+    CounterFunction()
+  },[counter])
+
+  function CounterFunction (){
+    console.log(`Counter ${counter}`)
+  }  
+ function callOnce(){
+    console.log("callOnce function called");
+  }
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <>
+  <h1>UseEffect Hooks</h1>
+  <button onClick={
+    ()=> {
+      setCounter(counter+1)
+    }
+  }>Counter {counter}</button>
+  <button onClick={
+    ()=> {
+      setData(data+1)
+    }
+  }>data {data}</button>
+  
+  </>
   )
 }
 
