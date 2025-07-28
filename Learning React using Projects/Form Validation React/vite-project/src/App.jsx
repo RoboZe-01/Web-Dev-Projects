7e053f3a47d0981999ec6c1cd8772e68c685942e
RoboZe <roboticsxzero@gmail.com>
2025-07-28 23:27:38 +0530

    Done for the day will do it tommorow

--- Learning React using Projects/Form Validation React/vite-project/src/App.jsx
+++ Learning React using Projects/Form Validation React/vite-project/src/App.jsx
@@ -6,7 +6,18 @@ function App() {
 
   return (
   <>
-  <h1 className='text-5xl bg-red'>Form validation and handeling in react</h1>
+    <div className='h-screen flex items-center justify-center'>
+
+      <div className='bg-white rounded-xl p-6 w-96'>
+        <form className='flex flex-col items-center'>
+          <input className = 'border-2 border-black px-3 py-1 text-xl rounded' type="text" placeholder='Enter Name here' />
+          <input className = 'border-2 border-black px-3 py-1 text-xl rounded' type="password" placeholder='Enter Password' />
+          <input className = 'border-2 border-black px-3 py-1 text-xl rounded' type="email" placeholder='Enter Your email' />
+          <input className = 'border-2 border-black px-3 py-1 text-xl rounded' type="password" placeholder='Confirm Password' />
+         
+        </form>
+      </div>
+    </div>
   </>
   )
 }
