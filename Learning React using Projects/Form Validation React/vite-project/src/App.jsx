471ccba09d8ce44b7fd652a53728609a0560a9c0
RoboZe <roboticsxzero@gmail.com>
2025-07-28 22:36:47 +0530

    Form Validation started in react

---
+++ Learning React using Projects/Form Validation React/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+  <>
+  <h1>Form validation and handeling in react</h1>
+  </>
+  )
+}
+
+export default App
