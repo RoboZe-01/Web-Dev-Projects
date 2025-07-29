f5ef62d273970ea1de100726a4380f38da539266
RoboZe <roboticsxzero@gmail.com>
2025-07-29 18:53:31 +0530

    React form setup

--- Learning React using Projects/Form Validation React/vite-project/src/App.jsx
+++ Learning React using Projects/Form Validation React/vite-project/src/App.jsx
@@ -11,9 +11,14 @@ function App() {
       <div className='bg-white rounded-xl p-6 w-96'>
         <form className='flex flex-col items-center'>
           <input className = 'border-2 border-black px-3 py-1 text-xl rounded' type="text" placeholder='Enter Name here' />
+
           <input className = 'border-2 border-black px-3 py-1 text-xl rounded' type="password" placeholder='Enter Password' />
+
           <input className = 'border-2 border-black px-3 py-1 text-xl rounded' type="email" placeholder='Enter Your email' />
+
           <input className = 'border-2 border-black px-3 py-1 text-xl rounded' type="password" placeholder='Confirm Password' />
+
+          <button className='text-xl px-4 rounded bg-emerald-500'></button>
          
         </form>
       </div>
