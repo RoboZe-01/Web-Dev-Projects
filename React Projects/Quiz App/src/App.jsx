cb01093f6af2f3f7431f7fa5b71357594aff857d
RoboZe <roboticsxzero@gmail.com>
2025-08-08 19:07:25 +0530

    Options added in the web

--- React Projects/Quiz App/src/App.jsx
+++ React Projects/Quiz App/src/App.jsx
@@ -3,14 +3,27 @@ import QuestionCard from "./components/QuestionCard"
 import { questions } from "./data/questions"
 
 function App() {
-  const [currentQuestion , setCurrentQuestion]=useState(0)
+  const [currentQuestion , setCurrentQuestion]=useState(0);
+  const [selectedAnswer , setSelectedAnswer]= useState(null);
+  const[score , setScore]= useState(0);
+  const[isFinished,setFinished]=useState(false);
+  const[showFeedback , setShowFeedback] = useState(false);
+
+  const handelAnswerr = (option) =>{
+    if (showFeedback) return;
+    setSelectedAnswer(option);
+    setShowFeedback(true);
+    if (option === questions[currentQuestion].answer){
+      setScore (score+1);
+    }
+  }
 
 
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center flex-col" >
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center flex-col p-4" >
      
-      <div className="text-center">
+      <div className="text-center mb-8">
         <h1 className="text-4xl font-bold text-purple-500 mb-2">Quiz App 
 
         </h1>
--- React Projects/Quiz App/src/components/QuestionCard.jsx
+++ React Projects/Quiz App/src/components/QuestionCard.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const QuestionCard = ( {data} ) => {
     const {question ,options, answer}= data;
   return (
-    <div className='bg-gray-800 p-6 rounded-2xl shadow-1g w-full max-w-xl border border-gray-700'>
-     questioncard {question}
+    <div className='bg-gray-800 p-6 rounded-2xl shadow-1g w-full max-w-xl border border-gray-700  '>
+     <p className='mb-5 text-xl'>{question}</p>
+     <div className='grid gap-3'>
+      {options.map((option,index) => (
+
+        <button className='text-left px-4 py-3 cursor-pointer bg-purple-400 rounded-xl text-amber-50' key={index}>
+             {option}
+        </button>
+      )) }
+     </div>
     </div>
   )
 }
