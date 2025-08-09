70b5d7d5127da79331f47c06e93e52865dc74d6a
RoboZe <roboticsxzero@gmail.com>
2025-08-09 13:49:29 +0530

    Showing saurav

---
+++ Website to show to saurvav/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <img src="./assets/WhatsApp Image 2025-08-09 at 13.46.34_b73391d6.jpg" alt="" />
+        <h1>This is saurav </h1>
+        <p>He is a Ba student</p>
+        <div>Box</div>
+      </div>
+    </>
+  )
+}
+
+export default App
